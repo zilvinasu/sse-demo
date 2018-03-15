835028dbcf2235282897ccb6ce899e176870a6a2
Žilvinas Urbonas <zilvinas.urbonas@inventi.lt>
2018-03-15 22:39:50 +0200

    feat(api): map publish request to data class

--- src/main/kotlin/sse/AppController.kt
+++ src/main/kotlin/sse/AppController.kt
@@ -2,6 +2,7 @@ package sse
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -13,7 +14,7 @@ class AppController() {
     fun health() = mapOf("status" to "UP")
 
     @PostMapping("/v1/events")
-    fun publish(event: Event) = sseService.publish(event)
+    fun publish(@RequestBody event: Event) = sseService.publish(event)
 
     @GetMapping("/v1/events:subscribe")
     fun subscribe() = sseService.subscribe()
