1686deda981e128ce83f96100017727158e5532d
Žilvinas Urbonas <zilvinas.urbonas@inventi.lt>
2018-03-16 13:52:07 +0200

    refactor(ui): cleanup the imports

--- ui/src/MessageList.js
+++ ui/src/MessageList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, ListItem, ListItemText, Divider } from 'material-ui';
+import { List, ListItem, ListItemText } from 'material-ui';
 
 const styles = {
   list: {
