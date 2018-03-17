522f241082f588b626f7b047249ae1046c32440b
Žilvinas Urbonas <zilvinas.urbonas@inventi.lt>
2018-03-17 11:46:27 +0200

    feat(server): ping back to the client

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const restify = require('restify');
 const server = restify.createServer();
 const events = require('events');
 const emitter = new events.EventEmitter();
-const EventType = { MessageReceived: 'MessageReceived' };
+const EventType = { MESSAGE_RECEIVED: 'MESSAGE_RECEIVED', PING: 'PING' };
 
-emitter.on(EventType.MessageReceived, (data) => {
-  console.log(`[${EventType.MessageReceived}]=>`, data);
-});
+setInterval(() => {
+  emitter.emit(EventType.PING)
+}, 3000);
 
 server.use(restify.plugins.bodyParser({ mapParams: false }));
 server.get('/health', (req, res, next) => {
@@ -15,22 +15,22 @@ server.get('/health', (req, res, next) => {
 });
 
 server.post('/v1/events', (req, res, next) => {
-  emitter.emit(EventType.MessageReceived, req.body);
-  res.setHeader('connection', 'close');
+  emitter.emit(EventType.MESSAGE_RECEIVED, req.body);
   res.send({ id: req.body.id });
-  next();
+  return next();
 });
 
 server.get('/v1/events:subscribe', (req, res, next) => {
   res.setHeader('content-type', 'text/event-stream');
   res.setHeader('cache-control', 'no-cache');
 
-  emitter.on(EventType.MessageReceived, (data) => {
+  emitter.on(EventType.MESSAGE_RECEIVED, (data) => {
     res.write(`${JSON.stringify(data)}\n\n`)
-  })
+  });
+
+  emitter.on(EventType.PING, () => res.write(`:PING\n\n`))
 });
 
-//TODO: Implement ping mechanism
 
 server.listen(8080, () => {
   console.log('%s listening at %s', server.name, server.url);
